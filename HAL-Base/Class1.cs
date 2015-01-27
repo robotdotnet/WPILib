499d23eba56a1122ab4675ce8d902d9e7a87b96b
Thad House <thad@athometech.com>
2015-01-27 14:55:20 -0800

    Added base projects

---
+++ HAL-Base/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HAL_Base
+{
+    public class Class1
+    {
+    }
+}
---
+++ HAL-RoboRIO/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HAL_RoboRIO
+{
+    public class Class1
+    {
+    }
+}
