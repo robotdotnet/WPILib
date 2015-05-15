6cad27786fca087f8eaf0a6e49f4520538550076
ThadHouse <thad@athometech.com>
2015-05-14 19:21:46 -0700

    Added Power and PDP

---
+++ WPILib/ControllerPower.cs
@@ -0,0 +1,106 @@
+﻿using HAL_FRC;
+
+namespace WPILib
+{
+    public class ControllerPower
+    {
+        public static double GetInputVoltage()
+        {
+            int status = 0;
+            double retVal = HALPower.getVinVoltage(ref status);
+            return retVal;
+        }
+
+        public static double GetInputCurrrent()
+        {
+            int status = 0;
+            double retVal = HALPower.getVinCurrent(ref status);
+            return retVal;
+        }
+
+        public static double GetVoltage3V3()
+        {
+            int status = 0;
+            double retVal = HALPower.getUserVoltage3V3(ref status);
+            return retVal;
+        }
+
+        public static double GetCurrent3V3()
+        {
+            int status = 0;
+            double retVal = HALPower.getUserCurrent3V3(ref status);
+            return retVal;
+        }
+
+        public static bool GetEnabled3V3()
+        {
+            int status = 0;
+            bool retVal = HALPower.getUserActive3V3(ref status);
+            return retVal;
+        }
+
+        public static int GetFaultCount3V3()
+        {
+            int status = 0;
+            int retVal = HALPower.getUserCurrentFaults3V3(ref status);
+            return retVal;
+        }
+
+        public static double GetVoltage5V()
+        {
+            int status = 0;
+            double retVal = HALPower.getUserVoltage5V(ref status);
+            return retVal;
+        }
+
+        public static double GetCurrent5V()
+        {
+            int status = 0;
+            double retVal = HALPower.getUserCurrent5V(ref status);
+            return retVal;
+        }
+
+        public static bool GetEnabled5V()
+        {
+            int status = 0;
+            bool retVal = HALPower.getUserActive5V(ref status);
+            return retVal;
+        }
+
+        public static int GetFaultCount5V()
+        {
+            int status = 0;
+            int retVal = HALPower.getUserCurrentFaults5V(ref status);
+            return retVal;
+        }
+
+        public static double GetVoltage6V()
+        {
+            int status = 0;
+            double retVal = HALPower.getUserVoltage6V(ref status);
+            return retVal;
+        }
+
+        public static double GetCurrent6V()
+        {
+            int status = 0;
+            double retVal = HALPower.getUserCurrent6V(ref status);
+            return retVal;
+        }
+
+        public static bool GetEnabled6V()
+        {
+            int status = 0;
+            bool retVal = HALPower.getUserActive6V(ref status);
+            return retVal;
+        }
+
+        public static int GetFaultCount6V()
+        {
+            int status = 0;
+            int retVal = HALPower.getUserCurrentFaults6V(ref status);
+            return retVal;
+        }
+
+    }
+}
---
+++ WPILib/PowerDistributionPanel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WPILib.Interfaces;
+using HAL_FRC;
+
+namespace WPILib
+{
+    public class PowerDistributionPanel : SensorBase
+    {
+        public PowerDistributionPanel()
+        {
+            
+        }
+
+        public double GetVoltage()
+        {
+            int status = 0;
+            double value = HALPDP.getPDPVoltage(ref status);
+            return value;
+        }
+
+        public double GetTemperature()
+        {
+            int status = 0;
+            double value = HALPDP.getPDPTemperature(ref status);
+            return value;
+        }
+
+        public double GetChannel(int channel)
+        {
+            int status = 0;
+            double value = HALPDP.getPDPChannelCurrent((byte)channel, ref status);
+            return value;
+        }
+
+        public double GetTotalCurrent()
+        {
+            int status = 0;
+            double value = HALPDP.getPDPTotalCurrent(ref status);
+            return value;
+        }
+
+        public double GetTotalPower()
+        {
+            int status = 0;
+            double value = HALPDP.getPDPTotalPower(ref status);
+            return value;
+        }
+
+        public double GetTotalEnergy()
+        {
+            int status = 0;
+            double value = HALPDP.getPDPTotalEnergy(ref status);
+            return value;
+        }
+
+        public void ResetEnergyTotal()
+        {
+            int status = 0;
+            HALPDP.resetPDPTotalEnergy(ref status);
+        }
+
+        public void ClearStickyFaults()
+        {
+            int status = 0;
+            HALPDP.clearPDPStickyFaults(ref status);
+        }
+
+        
+    }
+}
