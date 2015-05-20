5d2c0e99ffe7c3b24864f5f46deaa1db7ae99855
ThadHouse <thad@athometech.com>
2015-05-20 00:48:40 -0700

    Started working on Adding commands
    Also added folders. The WPILib project needs to have the same file
    format as the WPILibJ.

---
+++ WPILib/Buttons/Trigger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WPILib.Buttons
+{
+    public abstract class Trigger : Sendable
+    {
+        public abstract bool Get();
+
+        private bool Grab()
+        {
+            return Get(); // Add table when we have it.
+        }
+    }
+}
---
+++ WPILib/Command/IllegalUseOfCommandException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WPILib.Command
+{
+    public class IllegalUseOfCommandException : SystemException
+    {
+        public IllegalUseOfCommandException()
+        {
+            
+        }
+
+        public IllegalUseOfCommandException(string message) : base(message)
+        {
+            
+        }
+    }
+}
