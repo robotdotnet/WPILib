e03caa7cb28c225cff35fcc2aa38e8c973d2b8af
ThadHouse <thad@athometech.com>
2015-05-20 19:55:24 -0700

    Should actually fix Build.bat

--- build.bat
+++ build.bat
@@ -10,8 +10,8 @@ if not "%PackageVersion%" == "" (
 )
 
 REM Build
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild robotdotnet-wpilib /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild robotdotnet-wpilib.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 REM Package
 mkdir Build
-cmd /c %nuget% pack "robotdotnet-wpilib\WPILib\WPILib.csproj" -IncludeReferencedProjects -o Build -p Configuration=%config% %version%
+cmd /c nuget.exe pack "WPILib\WPILib.csproj" -IncludeReferencedProjects -o Build -p Configuration=%config% %version%
