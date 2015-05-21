d60c86841c506205d25de7c6cb51f30dad8afab6
ThadHouse <thad@athometech.com>
2015-05-20 19:44:16 -0700

    Add build.bat

---
+++ build.bat
@@ -0,0 +1,17 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+
+set version=
+if not "%PackageVersion%" == "" (
+   set version=-Version %PackageVersion%
+)
+
+REM Build
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild robotdotnet-wpilib /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+
+REM Package
+mkdir Build
+cmd /c %nuget% pack "robotdotnet-wpilib\WPILib.csproj" -IncludeReferencedProjects -o Build -p Configuration=%config% %version%
