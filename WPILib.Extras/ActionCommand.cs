3fd12516433cd7aa07bb4924fc1bc586a85892cd
Jeremy Koritzinsky <jkoritzinsky@live.com>
2015-08-02 14:12:36 -0500

    Add the ActionCommand class.  This class will allow teams to utilize the Button class events even if they aren't using the full Command-Subsystem model.

---
+++ WPILib.Extras/ActionCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPILib.Extras
+{
+    public class ActionCommand : Commands.Command
+    {
+        private Action action;
+
+        public ActionCommand(Action action)
+        {
+            this.action = action;
+        }
+
+        public ActionCommand(Action action, string name)
+            :base(name)
+        {
+            this.action = action;
+        }
+
+        protected override void End()
+        {
+        }
+
+        protected override void Execute()
+        {
+        }
+
+        protected override void Initialize()
+        {
+            action();
+        }
+
+        protected override void Interrupted()
+        {
+        }
+
+        protected override bool IsFinished()
+        {
+            return true;
+        }
+    }
+}
