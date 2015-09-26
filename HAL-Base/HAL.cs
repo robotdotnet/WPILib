3fb81c622aed3bbbd36805c9c3b1b1be4294eebb
Thad House <thadhouse1@gmail.com>
2015-09-25 18:27:45 -0700

    With the DLL name changes, fixes plugins.

--- HAL-Base/HAL.cs
+++ HAL-Base/HAL.cs
@@ -164,9 +164,9 @@ namespace HAL_Base
         {
             //Get a list of all dll files
             string[] dllFileNames = null;
-            if (Directory.Exists(Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + "Plugins"))
+            if (Directory.Exists(Directory.GetCurrentDirectory()))
             {
-                dllFileNames = Directory.GetFiles(Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + "Plugins", "*.dll");
+                dllFileNames = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.dll");
             }
 
             //If files not found, just return
