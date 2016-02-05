3b2975afad5a37ed8d1afe16416ebc82ca31c596
Thad House <thadhouse1@gmail.com>
2016-02-05 14:26:28 -0800

    Fixes Solenoid Memory Leak
    HAL ports can be freed after solenoid port is created

--- WPILib/SolenoidBase.cs
+++ WPILib/SolenoidBase.cs
@@ -33,8 +33,10 @@ namespace WPILib
             {
                 ModuleNumber = moduleNumber;
                 int status = 0;
-                m_port = InitializeSolenoidPort(GetPortWithModule((byte) ModuleNumber, 0), ref status);
+                IntPtr port = GetPortWithModule((byte)ModuleNumber, 0);
+                m_port = InitializeSolenoidPort(port, ref status);
                 CheckStatus(status);
+                FreePort(port);
             }
         }
 
