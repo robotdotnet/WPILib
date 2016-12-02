25d76b15a17f1da27bd33de4eb0d2e972784135e
Thad House <thadhouse1@gmail.com>
2016-12-02 13:01:47 -0800

    Fixes packaging to happen before deploy

--- assemble.ps1
+++ assemble.ps1
@@ -1,5 +1,6 @@
 param (
   [switch]$build = $false,
+  [switch]$test = $false,
   [switch]$pack = $false
 )
 
@@ -33,6 +34,38 @@ echo $buildNumber
 if ($build) {
   ./NuGet restore
   ./gradlew build -PbuildVersion="$version" -PbuildType="$type" -PbuildNumber="$buildNumber"
+}
+
+function Exec  
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
+        [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
+    )
+    & $cmd
+    if ($lastexitcode -ne 0) {
+        throw ("Exec: " + $errorMessage)
+    }
+}
+
+if ($test) {
+  $OpenCoverVersion = "4.6.519"
+    
+  $openCoverRun = ".\buildTemp\OpenCover.$OpenCoverVersion\tools\OpenCover.Console.exe"
+  
+  
+  # install CodeCov
+  .\NuGet.exe install OpenCover -Version $OpenCoverVersion -OutputDirectory buildTemp
+  
+  exec { & $openCoverRun -register:user -target:nunit3-console.exe -targetargs:".\WPILib.Tests\Output\WPILib.Tests.dll --framework=net-4.5 " -filter:"+[WPILib*]* -[HAL*]* -[WPILib.T*]* -[WPILib.IntegrationT*]* -[NIVision*]*" -output:coverage.xml -mergeoutput -returntargetcode }
+  exec { & $openCoverRun -register:user -target:nunit3-console.exe -targetargs:".\WPILib.IntegrationTests\Output\WPILib.IntegrationTests.dll --framework=net-4.5 " -filter:"+[WPILib*]* -[HAL*]* -[WPILib.T*]* -[WPILib.IntegrationT*]* -[NIVision*]*" -output:coverageIntegration.xml }
+  
+  $env:Path = "C:\Python34;C:\\Python34\Scripts;" + $env:Path
+  
+  & pip install codecov
+  
+  & codecov -f "coverage.xml"
 }
 
 if ($pack) {
