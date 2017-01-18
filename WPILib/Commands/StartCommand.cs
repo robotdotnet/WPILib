813be83102bb97d9d95593377c7b9538019ba88e
Matt Soucy <msoucy@csh.rit.edu>
2017-01-17 22:47:51 -0500

    Add implementations of the new 2017 commands (#113)
    Added `ConditionalCommand`, `InstantCommand`, and `TimedCommand`.
    
    Reworked hierarchy to reflect WPILibJ's version
    
    Converted abstract methods to virtual, to be more inline with WPILibJ.

--- WPILib/Commands/StartCommand.cs
+++ WPILib/Commands/StartCommand.cs
@@ -4,12 +4,12 @@
     /// A <see cref="StartCommand"/> will call the <see cref="Command.Start">Start()</see> method of another
     /// command when it is initialized and will finish immediately.
     /// </summary>
-    public class StartCommand : Command
+    public class StartCommand : InstantCommand
     {
         private readonly Command m_commandToFork;
 
         /// <summary>
-        /// Instantiates a <see cref="StartCommand"/> which will start the given command when its 
+        /// Instantiates a <see cref="StartCommand"/> which will start the given command when its
         /// <see cref="Initialize"/> is called.
         /// </summary>
         /// <param name="commandToStart">The <see cref="Command"/> to start.</param>
@@ -17,23 +17,8 @@
         {
             m_commandToFork = commandToStart;
         }
+
         ///<inheritdoc/>
         protected override void Initialize() => m_commandToFork.Start();
-
-        ///<inheritdoc/>
-        protected override void Execute()
-        {
-        }
-        ///<inheritdoc/>
-        protected override bool IsFinished() => true;
-
-        ///<inheritdoc/>
-        protected override void End()
-        {
-        }
-        ///<inheritdoc/>
-        protected override void Interrupted()
-        {
-        }
     }
 }
---
+++ WPILib/Commands/TimedCommand.cs
@@ -0,0 +1,18 @@
+﻿namespace WPILib.Commands
+{
+    /// <summary>
+    /// This command will wait for a timeout before finishing
+    /// </summary>
+    public class TimedCommand : Command
+    {
+        public TimedCommand(double timeout) : base(timeout) { }
+
+        /// <summary>
+        /// Creates a new <see cref="TimedCommand"/> with the given name
+        /// </summary>
+        public TimedCommand(string name, double timeout) : base(name, timeout) { }
+
+        ///<inheritdoc/>
+        protected override bool IsFinished() => IsTimedOut();
+    }
+}
