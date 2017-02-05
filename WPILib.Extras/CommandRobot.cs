1479842f7cc5322808dc87eeae33366328c30b51
Matt Soucy <msoucy@csh.rit.edu>
2017-02-05 13:53:15 -0500

    Add a CommandRobot base
    Takes care of the boilerplate for running the scheduler.
    Does not implement any of the logic for selecting or creating autonomous modes.

---
+++ WPILib.Extras/CommandRobot.cs
@@ -0,0 +1,26 @@
+﻿using WPILib.Commands;
+
+namespace WPILib.Extras
+{
+    /// <summary>
+    /// Implements the boilerplate needed to use an <see cref="IterativeRobot"/>
+    /// with the command-based model, by running the scheduler as needed
+    /// </summary>
+    public abstract class CommandRobot : IterativeRobot
+    {
+        // This function is called periodically while disabled
+        public override void DisabledPeriodic() => Scheduler.Instance.Run();
+
+        // This function is called periodically during autonomous
+        public override void AutonomousPeriodic() => Scheduler.Instance.Run();
+
+        // This function is called when the disabled button is hit.
+        public override void DisabledInit() { }
+
+        // This function is called periodically during operator control
+        public override void TeleopPeriodic() => Scheduler.Instance.Run();
+
+        // This function is called periodically during test mode
+        public override void TestPeriodic() => LiveWindow.LiveWindow.Run();
+    }
+}
