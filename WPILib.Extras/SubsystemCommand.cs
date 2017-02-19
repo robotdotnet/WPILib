62ce5dc8f143340ce316dd97e897249b9d3ab56d
Matt Soucy <msoucy@csh.rit.edu>
2017-02-18 21:24:05 -0500

    Additional support for command-based robots (#116)
    * Add a CommandRobot base
    
    Takes care of the boilerplate for running the scheduler.
    Does not implement any of the logic for selecting or creating autonomous modes.
    
    * Add SubsystemCommand to extras
    
    It behaves as a regular command, but automatically calls `.Requires` on the provided subsystem and stores a reference to it as `m_subsystem`
    
    * Fix comments to provide more accurate description

---
+++ WPILib.Extras/SubsystemCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using WPILib.Commands;
+
+namespace WPILib.Extras
+{
+    /// <summary>
+    /// A <see cref="SubsystemCommand"/> depends on the given <see cref="Subsystem"/>.
+    /// It stores a local reference to its <see cref="Subsystem" /> to ease access.
+    /// </summary>
+    public abstract class SubsystemCommand : Command
+    {
+        protected readonly Subsystem m_subsystem;
+
+        /// <summary>
+        /// Creates a new <see cref="SubsystemCommand"/> which will depend on the given <see cref="Subsystem"/>.
+        /// </summary>
+        /// <param name="action">The <see cref="Action"/> to run.</param>
+        public SubsystemCommand(Subsystem subsystem)
+        {
+            Requires(subsystem);
+            m_subsystem = subsystem;
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="SubsystemCommand"/> with a specific name, 
+        /// which will depend on the given <see cref="Subsystem"/>.
+        /// </summary>
+        /// <param name="subsystem">The <see cref="Subsystem"/> to require.</param>
+        /// <param name="name">The name for the command.</param>
+        public SubsystemCommand(Subsystem subsystem, string name)
+            : base(name)
+        {
+            Requires(subsystem);
+            m_subsystem = subsystem;
+        }
+    }
+}
