e79296d2b9bca964fd1a57f5ef427f54bb14ad7f
Thad House <thadhouse1@gmail.com>
2020-01-23 15:30:33 -0800

    More updates to some build and test infrastructure

---
+++ dev/desktopDev/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace desktopDev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
