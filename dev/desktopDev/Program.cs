f7a3be38ff6e2932dc4efdf1a7259ac85d035c2e
Thad House <thadhouse1@gmail.com>
2020-01-23 17:22:04 -0800

    Work on adding spark max support

--- dev/desktopDev/Program.cs
+++ dev/desktopDev/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using REV.SparkMax.Natives;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
 
 namespace desktopDev
 {
@@ -7,6 +11,98 @@ namespace desktopDev
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var methods = typeof(ICANSparkMaxDriver).GetMethods();
+
+            StringBuilder builder = new StringBuilder();
+
+            foreach (var method in methods)
+            {
+                if (method.ReturnType == typeof(void))
+                {
+                    WriteVoidMethod(method, builder);
+                }
+                else
+                {
+                    WriteReturningMethod(method, builder);
+                }
+            }
+            File.WriteAllText("methods.txt", builder.ToString());
+            ;
+        }
+
+        static string ToCSharpName(Type type)
+        {
+            if (type == typeof(void*))
+            {
+                return "void*";
+            }
+            return type.Name;
+        }
+
+        static void WriteParameterList(MethodInfo info, StringBuilder builder)
+        {
+            bool first = true;
+            foreach (var parameter in info.GetParameters())
+            {
+                if (first)
+                {
+                    first = false;
+                }
+                else
+                {
+                    builder.Append(", ");
+                }
+
+                builder.Append($"{ToCSharpName(parameter.ParameterType)} {parameter.Name}");
+            }
+            builder.AppendLine(")");
+        }
+
+        static void WriteCallParameterList(MethodInfo info, StringBuilder builder)
+        {
+            bool first = true;
+            foreach (var parameter in info.GetParameters())
+            {
+                if (first)
+                {
+                    first = false;
+                }
+                else
+                {
+                    builder.Append(", ");
+                }
+
+                builder.Append($"{parameter.Name}");
+            }
+            builder.AppendLine(");");
+        }
+
+        static void WriteVoidMethod(MethodInfo info, StringBuilder builder)
+        {
+            builder.Append($"public static void {info.Name.Replace("c_SparkMax_", "")}(");
+            WriteParameterList(info, builder);
+            builder.Append("{");
+            builder.AppendLine($"  driver.{info.Name}(");
+            WriteCallParameterList(info, builder);
+
+            builder.AppendLine("}");
+
+        }
+
+        static void WriteReturningMethod(MethodInfo info, StringBuilder builder)
+        {
+            builder.Append($"public static {ToCSharpName(info.ReturnType)} {info.Name.Replace("c_SparkMax_", "")}(");
+            WriteParameterList(info, builder);
+
+
+
+            builder.AppendLine("{");
+
+            builder.Append($"  return driver.{info.Name}(");
+            WriteCallParameterList(info, builder);
+
+            builder.AppendLine("}");
         }
     }
 }
---
+++ src/thirdparty/SparkMax/Natives/SparkMaxDriverTypes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace REV.SparkMax.Natives
+{
+    public struct PeriodicStatus0LowLevel
+    {
+        public float AppliedOutput;
+        public ushort Faults;
+        public ushort StickyFaults;
+        public MotorType MotorType;
+        public byte IsFollower;
+        public byte IsInverted;
+        public byte Lock;
+        public byte RoboRIO;
+        public ulong Timestamp;
+    }
+
+    public struct PeriodicStatus1LowLevel
+    {
+        public float SensorVelocity;
+        public byte MotorTemperature;
+        public float BusVoltage;
+        public float OutputCurrrent;
+        public ulong Timestamp;
+    }
+
+    public struct PeriodicStatus2LowLevel
+    {
+        public float SensorPostion;
+        public float IAccum;
+        public ulong Timestamp;
+    }
+
+    public struct PeriodicStatus3LowLevel
+    {
+        public float AnalogVoltage;
+        public float AnalogVelocity;
+        public float AnalogPosition;
+        public ulong Timestamp;
+    }
+
+    public struct PeriodicStatus4LowLevel
+    {
+        public float AltEncoderPosition;
+        public float AltEncoderVelocity;
+        public ulong Timestamp;
+    }
+}
