d1945823e2d7f41883caa3e5d4d74d886cb7d68f
Thad House <thadhouse1@gmail.com>
2020-02-18 11:18:13 -0800

    A bit more work on oblog

---
+++ src/thirdparty/oblog/Attributes/ConfigAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPILib.Oblog.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Method | AttributeTargets.Parameter | AttributeTargets.Property, AllowMultiple = true)]
+    public class ConfigAttribute : Attribute
+    {
+        public string Name { get; set; } = "NO_NAME";
+        public string TabName { get; set; } = "DEFAULT";
+
+        public string MethodName { get; set; } = "DEFAULT";
+
+
+    }
+}
