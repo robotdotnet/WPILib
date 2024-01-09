1a050929e361d7bcbc0e4969b40be48dc79e93ee
Thad House <thadhouse1@gmail.com>
2024-01-08 18:18:40 -0800

    Start adding synchronization.

---
+++ src/wpiutil/Concurrent/Event.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using WPIUtil.Natives;
+
+namespace WPIUtil.Concurrent;
+
+public sealed class Event(bool manualReset = false, bool initialState = false) : IDisposable
+{
+    public int Handle { get; private set; } = SynchronizationNative.CreateEvent(manualReset, initialState);
+
+    public void Dispose()
+    {
+        if (Handle != 0)
+        {
+            SynchronizationNative.DestroyEvent(Handle);
+            Handle = 0;
+        }
+    }
+
+    public void Set()
+    {
+        SynchronizationNative.SetEvent(Handle);
+    }
+
+    public void Reset()
+    {
+        SynchronizationNative.ResetEvent(Handle);
+    }
+}
---
+++ src/wpiutil/Concurrent/Synchronization.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using CommunityToolkit.Diagnostics;
+using WPIUtil.Natives;
+
+namespace WPIUtil.Concurrent;
+
+public static class Synchronization
+{
+    public static SynchronizationResult WaitForObject(int handle)
+    {
+        return SynchronizationNative.WaitForObject(handle) ? SynchronizationResult.Signaled : SynchronizationResult.Cancelled;
+    }
+
+    public static SynchronizationResult WaitForObject(int handle, TimeSpan timeout)
+    {
+        bool signaled = SynchronizationNative.WaitForObjectTimeout(handle, timeout.TotalSeconds, out var timedOut);
+        if (signaled)
+        {
+            return SynchronizationResult.Signaled;
+        }
+        else if (timedOut)
+        {
+            return SynchronizationResult.TimedOut;
+        }
+        else
+        {
+            return SynchronizationResult.Cancelled;
+        }
+    }
+
+    public static ReadOnlySpan<int> WaitForObjects(ReadOnlySpan<int> handles, Span<int> signaled)
+    {
+        if (signaled.Length < handles.Length)
+        {
+            ThrowHelper.ThrowArgumentOutOfRangeException($"{nameof(signaled)} must have at least as much capacity as {nameof(handles)}");
+        }
+
+        int written = SynchronizationNative.WaitForObjects(handles, handles.Length, signaled);
+        return signaled[..written];
+    }
+
+    public static ReadOnlySpan<int> WaitForObjects(ReadOnlySpan<int> handles, Span<int> signaled, TimeSpan timeout, out bool timedOut)
+    {
+        if (signaled.Length < handles.Length)
+        {
+            ThrowHelper.ThrowArgumentOutOfRangeException($"{nameof(signaled)} must have at least as much capacity as {nameof(handles)}");
+        }
+
+        int written = SynchronizationNative.WaitForObjectsTimeout(handles, handles.Length, signaled, timeout.TotalSeconds, out timedOut);
+        return signaled[..written];
+    }
+}
