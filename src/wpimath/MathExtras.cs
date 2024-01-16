b2baa4f2fc3d5626180ec66ff952e2d1347b6095
Thad House <thadhouse1@gmail.com>
2024-01-16 07:40:52 -0800

    Format and editorconfig line endings

--- src/wpimath/MathExtras.cs
+++ src/wpimath/MathExtras.cs
@@ -2,10 +2,12 @@ using System.Numerics;
 
 namespace WPIMath;
 
-public static class MathExtras {
+public static class MathExtras
+{
     public static T Lerp<T>(T startValue, T endValue, double t) where T : IAdditionOperators<T, T, T>,
                                                                           ISubtractionOperators<T, T, T>,
-                                                                          IMultiplyOperators<T, double, T>  {
+                                                                          IMultiplyOperators<T, double, T>
+    {
         return startValue + (endValue - startValue) * t;
     }
 }
