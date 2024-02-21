7d4af4627c2cb399be36052325abebc18fc97063
Thad House <thadhouse1@gmail.com>
2024-02-21 13:58:32 -0800

    Move all TypeDeclarationModels

--- codehelp/CodeHelpers/SymbolExtensions.cs
+++ codehelp/CodeHelpers/SymbolExtensions.cs
@@ -8,33 +8,4 @@ public static class SymbolExtensions
     {
         return symbol.TypeKind == TypeKind.Pointer || symbol.TypeKind == TypeKind.FunctionPointer;
     }
-
-    public static TypeDeclarationModel GetTypeDeclarationModel(this INamedTypeSymbol symbol)
-    {
-        TypeModifiers modifiers = TypeModifiers.None;
-        if (symbol.IsReadOnly)
-        {
-            modifiers |= TypeModifiers.IsReadOnly;
-        }
-        if (symbol.IsRefLikeType)
-        {
-            modifiers |= TypeModifiers.IsRefLikeType;
-        }
-        if (symbol.IsRecord)
-        {
-            modifiers |= TypeModifiers.IsRecord;
-        }
-
-        var className = symbol.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat);
-        string? nspace = null;
-
-        TypeDeclarationModel? parent = null;
-        if (symbol.ContainingType is not null) {
-            parent = symbol.ContainingType.GetTypeDeclarationModel();
-        } else {
-            nspace = symbol.ContainingNamespace is { IsGlobalNamespace: false } ns ? ns.ToDisplayString() : null;
-        }
-
-        return new(symbol.TypeKind, modifiers, className, nspace, parent);
-    }
 }
--- codehelp/CodeHelpers/TypeDeclarationModel.cs
+++ codehelp/CodeHelpers/TypeDeclarationModel.cs
@@ -50,3 +50,34 @@ public record TypeDeclarationModel(TypeKind Kind, TypeModifiers Modifiers, strin
         return builder.EnterScope(count);
     } 
 }
+
+public static class TypeDeclarationExtensions {
+        public static TypeDeclarationModel GetTypeDeclarationModel(this INamedTypeSymbol symbol)
+    {
+        TypeModifiers modifiers = TypeModifiers.None;
+        if (symbol.IsReadOnly)
+        {
+            modifiers |= TypeModifiers.IsReadOnly;
+        }
+        if (symbol.IsRefLikeType)
+        {
+            modifiers |= TypeModifiers.IsRefLikeType;
+        }
+        if (symbol.IsRecord)
+        {
+            modifiers |= TypeModifiers.IsRecord;
+        }
+
+        var className = symbol.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat);
+        string? nspace = null;
+
+        TypeDeclarationModel? parent = null;
+        if (symbol.ContainingType is not null) {
+            parent = symbol.ContainingType.GetTypeDeclarationModel();
+        } else {
+            nspace = symbol.ContainingNamespace is { IsGlobalNamespace: false } ns ? ns.ToDisplayString() : null;
+        }
+
+        return new(symbol.TypeKind, modifiers, className, nspace, parent);
+    }
+}
