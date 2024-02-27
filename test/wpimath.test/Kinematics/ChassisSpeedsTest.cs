268fa85b4dec34ef8d62a68cf8d7762a9dac45bf
DeltaDizzy <EJBraswell@gmail.com>
2024-02-27 05:14:44 -0500

    start tests

---
+++ test/wpimath.test/Kinematics/ChassisSpeedsTest.cs
@@ -0,0 +1,44 @@
+﻿using UnitsNet;
+using UnitsNet.NumberExtensions.NumberToFrequency;
+using UnitsNet.NumberExtensions.NumberToSpeed;
+using WPIMath;
+using Xunit;
+
+namespace wpimath.test;
+
+public class ChassisSpeedsTest
+{
+    // test:
+    // all operators
+    // robot relative
+    // field relative
+    // serialize
+    // deserialize
+    // tostring
+
+    private static readonly double Epsilon = 1e-9;
+
+    [Fact]
+    public void TestAddition()
+    {
+        // Given
+        ChassisSpeeds first = new(1.MetersPerSecond(), 2.MetersPerSecond(), RotationalSpeed.FromRadiansPerSecond(3));
+        ChassisSpeeds second = new(4.MetersPerSecond(), 5.MetersPerSecond(), RotationalSpeed.FromRadiansPerSecond(6));
+        // When
+        ChassisSpeeds sum = first + second;
+        // Then
+        Assert.Equal(first.Vx.MetersPerSecond + second.Vx.MetersPerSecond, sum.Vx.MetersPerSecond, Epsilon);
+        Assert.Equal(first.Vy.MetersPerSecond + second.Vy.MetersPerSecond, sum.Vy.MetersPerSecond, Epsilon);
+        Assert.Equal(first.Omega.RadiansPerSecond + second.Omega.RadiansPerSecond, sum.Omega.RadiansPerSecond, Epsilon);
+    }
+
+    [Fact]
+    public void TestMinus()
+    {
+        // Given
+    
+        // When
+    
+        // Then
+    }
+}
